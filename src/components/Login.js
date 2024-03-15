7dd894ebd6e5e20cae04bf6a4044591a786262f9
Siddarth Koul <siddarthkoul21@gmail.com>
2024-03-15 21:53:41 +0530

    updates in login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
-import logo from '../Images/live-chat.png';
-import { Backdrop, CircularProgress, Button, TextField } from "@mui/material";
-import { useNavigate,} from 'react-router-dom';
-import axios from 'axios';
-import Toaster from './toaster';
+import React, { useState } from "react";
+import logo from "../Images/live-chat_512px.png";
+import { Backdrop, Button, CircularProgress, TextField } from "@mui/material";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import Toaster from "./Toaster";
 
 function Login() {
-  const [showLogin, setShowLogin] = useState(false);
+  const [showlogin, setShowLogin] = useState(false);
   const [data, setData] = useState({ name: "", email: "", password: "" });
   const [loading, setLoading] = useState(false);
 
   const [logInStatus, setLogInStatus] = React.useState("");
   const [signInStatus, setSignInStatus] = React.useState("");
-  let navigate = useNavigate();
+
+  const navigate = useNavigate();
 
   const changeHandler = (e) => {
     setData({ ...data, [e.target.name]: e.target.value });
@@ -24,11 +25,12 @@ function Login() {
     try {
       const config = {
         headers: {
-          'Content-Type': 'application/json',
+          "Content-type": "application/json",
         },
       };
+
       const response = await axios.post(
-        "http://localhost:8080/user/login",
+        "http://localhost:8080/user/login/",
         data,
         config
       );
@@ -37,14 +39,13 @@ function Login() {
       setLoading(false);
       localStorage.setItem("userData", JSON.stringify(response));
       navigate("/app/welcome");
-    }
-    catch (error) {
+    } catch (error) {
       setLogInStatus({
-        msg: "Invalid Username or Password",
+        msg: "Invalid User name or Password",
         key: Math.random(),
       });
-      setLoading(false);
     }
+    setLoading(false);
   };
 
   const signUpHandler = async () => {
@@ -52,80 +53,94 @@ function Login() {
     try {
       const config = {
         headers: {
-          'Content-Type': 'application/json',
+          "Content-type": "application/json",
         },
       };
 
       const response = await axios.post(
-        "http://localhost:8080/user/signup",
+        "http://localhost:8080/user/register/",
         data,
         config
       );
-      console.log(response)
+      console.log(response);
       setSignInStatus({ msg: "Success", key: Math.random() });
       navigate("/app/welcome");
       localStorage.setItem("userData", JSON.stringify(response));
+      setLoading(false);
     } catch (error) {
       console.log(error);
       if (error.response.status === 405) {
         setLogInStatus({
-          msg: "User with this email ID already exists",
+          msg: "User with this email ID already Exists",
           key: Math.random(),
         });
       }
       if (error.response.status === 406) {
         setLogInStatus({
-          msg: "User Name already taken,  please choose another one.",
+          msg: "User Name already Taken, Please take another one",
           key: Math.random(),
         });
       }
+      setLoading(false);
     }
-    setLoading(false);
   };
 
   return (
     <>
       <Backdrop
-        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
         open={loading}
       >
         <CircularProgress color="secondary" />
       </Backdrop>
-      <div className='login-container'>
-        <div className='image-container'>
-          <img src={logo} alt="logo" className='welcome-logo' />
+      <div className="login-container">
+        <div className="image-container">
+          <img src={logo} alt="Logo" className="welcome-logo" />
         </div>
-        {showLogin && (
-          <div className='login-box'>
-            <p className='login-text'>Login to your account</p>
+        {showlogin && (
+          <div className="login-box">
+            <p className="login-text">Login to your Account</p>
             <TextField
               onChange={changeHandler}
-              id='standard-basic'
-              label='Enter Username'
-              variant='outlined'
-              color='secondary'
-              name='name'
+              id="standard-basic"
+              label="Enter User Name"
+              variant="outlined"
+              color="secondary"
+              name="name"
+              onKeyDown={(event) => {
+                if (event.code == "Enter") {
+                  // console.log(event);
+                  loginHandler();
+                }
+              }}
             />
             <TextField
               onChange={changeHandler}
-              id='outlined-password-input'
-              label='Password'
-              type='password'
-              autoComplete='current-password'
-              color='secondary'
-              name='password'
+              id="outlined-password-input"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+              color="secondary"
+              name="password"
+              onKeyDown={(event) => {
+                if (event.code == "Enter") {
+                  // console.log(event);
+                  loginHandler();
+                }
+              }}
             />
             <Button
-              variant='outlined'
-              color='secondary'
+              variant="outlined"
+              color="secondary"
               onClick={loginHandler}
+              isLoading
             >
               Login
             </Button>
             <p>
-              Don't have an Account? { ' ' }
+              Don't have an Account ?{" "}
               <span
-                className='hyper'
+                className="hyper"
                 onClick={() => {
                   setShowLogin(false);
                 }}
@@ -133,34 +148,61 @@ function Login() {
                 Sign Up
               </span>
             </p>
-            {logInStatus ? (<Toaster key={logInStatus.key} message={logInStatus.msg} />
+            {logInStatus ? (
+              <Toaster key={logInStatus.key} message={logInStatus.msg} />
             ) : null}
           </div>
         )}
-        {!showLogin && (
-          <div className='login-box'>
-            <p className='login-text'>Create your Account</p>
+        {!showlogin && (
+          <div className="login-box">
+            <p className="login-text">Create your Account</p>
             <TextField
               onChange={changeHandler}
-              id='standard-basic'
-              label='Enter Username'
-              variant='outlined'
-              color='secondary'
-              name='name'
+              id="standard-basic"
+              label="Enter User Name"
+              variant="outlined"
+              color="secondary"
+              name="name"
               helperText=""
+              onKeyDown={(event) => {
+                if (event.code == "Enter") {
+                  // console.log(event);
+                  signUpHandler();
+                }
+              }}
             />
             <TextField
               onChange={changeHandler}
-              id='outlined-password-input'
-              label='Password'
-              type='password'
-              autoComplete='current-password'
-              color='secondary'
-              name='password'
+              id="standard-basic"
+              label="Enter Email Address"
+              variant="outlined"
+              color="secondary"
+              name="email"
+              onKeyDown={(event) => {
+                if (event.code == "Enter") {
+                  // console.log(event);
+                  signUpHandler();
+                }
+              }}
+            />
+            <TextField
+              onChange={changeHandler}
+              id="outlined-password-input"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+              color="secondary"
+              name="password"
+              onKeyDown={(event) => {
+                if (event.code == "Enter") {
+                  // console.log(event);
+                  signUpHandler();
+                }
+              }}
             />
             <Button
-              variant='outlined'
-              color='secondary'
+              variant="outlined"
+              color="secondary"
               onClick={signUpHandler}
             >
               Sign Up
@@ -168,15 +210,16 @@ function Login() {
             <p>
               Already have an Account ?
               <span
-                className='hyper'
+                className="hyper"
                 onClick={() => {
-                  setShowLogin(true)
-                }} 
+                  setShowLogin(true);
+                }}
               >
                 Log in
               </span>
             </p>
-            {signInStatus? (<Toaster key={signInStatus.key} message={signInStatus.msg} />
+            {signInStatus ? (
+              <Toaster key={signInStatus.key} message={signInStatus.msg} />
             ) : null}
           </div>
         )}
